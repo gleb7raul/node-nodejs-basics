abe7a5255ca470a9ac4005b6cc4965e9be15374a
Hleb Nebiadukhin <hlebnebiadukhin@MacBook-Pro-Hleb.local>
2025-04-24 22:12:15 +0200

    Add tests

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+
+const FILE_PATH = './files/fileToCalculateHashFor.txt';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = createHash('sha256');
+    const stream = createReadStream(FILE_PATH);
+
+    stream.on('data', (data) => {
+        hash.update(data);
+    });
+
+    stream.on('end', () => {
+        console.log(`SHA256 hash of ${FILE_PATH} is: ${hash.digest('hex')}`);
+    });
+
+    stream.on('error', (err) => {
+        console.error(`Error reading file: ${err.message}`);
+    });
 };
 
 await calculateHash();
